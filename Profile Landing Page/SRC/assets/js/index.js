0660d3d2b85b36d89875227a44fa6e2f97c0776f
T-N9 <tenyainmoelwin@ucsh.edu.mm>
2020-11-25 21:35:19 +0630

    nav-link remove

--- Profile Landing Page/SRC/assets/js/index.js
+++ Profile Landing Page/SRC/assets/js/index.js
@@ -1,34 +1,38 @@
 $(document).ready(function(){
     $('.navbar-toggler').click(function(){
-        // $('.my-md-menu').toggleClass('d-none');
         $('.my-md-menu').toggleClass('my-menu-animation-in');
         $('.my-md-menu').toggleClass('my-menu-animation-out');
     });
+    $('.nav-md-link').click(function(){
+        $('.my-md-menu').addClass('my-menu-animation-in');
+        $('.my-md-menu').removeClass('my-menu-animation-out');
+    });
+    
 // new WOW().init();
 
     let screenHeight=$(window).height();
     // console.log(screenHeight);
 
-    $(window).scroll(function(){
-        let currentPos=$(this).scrollTop();
-        // console.log(currentPos);
-        if(currentPos>=screenHeight-300){
-            console.log("hello");
-        }
-        else{
-            $(".topPage").removeClass('d-block');
-            $(".topPage").addClass('d-none');
-        }
-    });
+    // $(window).scroll(function(){
+    //     let currentPos=$(this).scrollTop();
+    //     // console.log(currentPos);
+    //     if(currentPos>=screenHeight-300){
+            
+    //     }
+    //     else{
+    //         $(".topPage").removeClass('d-block');
+    //         $(".topPage").addClass('d-none');
+    //     }
+    // });
 
-    var waypoint = new Waypoint({
-        element: document.getElementById('about'),
-        handler: function(direction) {
-          $(".topPage").removeClass('d-none');
-          $(".topPage").addClass('d-block');
-        },
-        offset: '0px'
-    });
+    // var waypoint = new Waypoint({
+    //     element: document.getElementById('about'),
+    //     handler: function(direction) {
+    //       $(".topPage").removeClass('d-none');
+    //       $(".topPage").addClass('d-block');
+    //     },
+    //     offset: '0px'
+    // });
 
     function navActive(current){
         $(`.nav-link`).removeClass("nav-active");
