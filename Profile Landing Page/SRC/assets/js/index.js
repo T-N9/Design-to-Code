4259421ed818af3964ac0de5094773d08705ae6c
T-N9 <tenyainmoelwin@ucsh.edu.mm>
2020-11-26 09:52:39 +0630

    updated

--- Profile Landing Page/SRC/assets/js/index.js
+++ Profile Landing Page/SRC/assets/js/index.js
@@ -7,33 +7,7 @@ $(document).ready(function(){
         $('.my-md-menu').addClass('my-menu-animation-in');
         $('.my-md-menu').removeClass('my-menu-animation-out');
     });
-    
     new WOW().init();
-
-    let screenHeight=$(window).height();
-    // console.log(screenHeight);
-
-    // $(window).scroll(function(){
-    //     let currentPos=$(this).scrollTop();
-    //     // console.log(currentPos);
-    //     if(currentPos>=screenHeight-300){
-            
-    //     }
-    //     else{
-    //         $(".topPage").removeClass('d-block');
-    //         $(".topPage").addClass('d-none');
-    //     }
-    // });
-
-    // var waypoint = new Waypoint({
-    //     element: document.getElementById('about'),
-    //     handler: function(direction) {
-    //       $(".topPage").removeClass('d-none');
-    //       $(".topPage").addClass('d-block');
-    //     },
-    //     offset: '0px'
-    // });
-
     function navActive(current){
         $(`.nav-link`).removeClass("nav-active");
         $(`.nav-link[href='#${current}']`).addClass("nav-active");
@@ -47,7 +21,6 @@ $(document).ready(function(){
         currentSec.waypoint(function(direction){
             if(direction=="down"){
                 let currentId=$(this.element).attr('id');
-                // console.log(currentId);
                 navActive(currentId);
                 navActive_md(currentId);
             }
@@ -55,7 +28,6 @@ $(document).ready(function(){
         currentSec.waypoint(function(direction){
             if(direction=="up"){
                 let currentId=$(this.element).attr('id');
-                // console.log(currentId);
                 navActive(currentId);
                 navActive_md(currentId);
             }
