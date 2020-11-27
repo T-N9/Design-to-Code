c8e544459662abc7a422a79f61f2c11b9d4f0837
T-N9 <tenyainmoelwin@ucsh.edu.mm>
2020-11-27 10:43:05 +0630

    nav-togger

--- Profile Landing Page/SRC/assets/js/index.js
+++ Profile Landing Page/SRC/assets/js/index.js
@@ -3,7 +3,16 @@ $(document).ready(function(){
     $('.navbar-toggler').click(function(){
         $('.my-md-menu').toggleClass('my-menu-animation-in');
         $('.my-md-menu').toggleClass('my-menu-animation-out');
+        let icon_con=$('.my-md-menu').hasClass('my-menu-animation-in');
+        if(icon_con){
+            $('#toggler-icon').removeClass('fa-times').addClass('fa-bars');
+           
+        }else{
+            $('#toggler-icon').removeClass('fa-bars').addClass('fa-times');
+        }
     });
+    
+    
     $('.nav-md-link').click(function(){
         $('.my-md-menu').addClass('my-menu-animation-in');
         $('.my-md-menu').removeClass('my-menu-animation-out');
