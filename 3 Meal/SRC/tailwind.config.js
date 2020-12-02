0e6e0704e2579bfe89cbb80fb911a7537564f9ed
T-N9 <tenyainmoelwin@ucsh.edu.mm>
2020-12-02 19:19:21 +0630

    updated projects

---
+++ 3 Meal/SRC/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        my_primary_color: '#D85A5A',
+        my_secondary_color:'#9E7474'
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
